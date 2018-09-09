648ab9b07fd63488caf8f4605b0e21bf9b554a63
bitbeast0101 <rohithch16@gmail.com>
2018-09-09 11:04:57 +0530

    Added files via upload

---
+++ ir_test.ino
@@ -0,0 +1,17 @@
+int sense = 10, obstacle = HIGH;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sense,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  obstacle = digitalRead(sense);
+  Serial.println(obstacle);
+  if ( obstacle == LOW) 
+    Serial.println("PATH CLEAR");
+  else
+    Serial.println("OBSTACLE DETECTED");
+  delay(500);
+}
