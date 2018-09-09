8d43a0ba2a00fdc0bc044e4eef18a7ddcaed79b0
bitbeast0101 <rohithch16@gmail.com>
2018-09-09 11:05:26 +0530

    Added files via upload

---
+++ motor_run.ino
@@ -0,0 +1,22 @@
+int r_in1 = 9, r_in2 = 6, l_in1 = 5, l_in2 = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(r_in1,OUTPUT);
+  pinMode(r_in2,OUTPUT);  
+  pinMode(l_in1,OUTPUT);
+  pinMode(l_in2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(r_in1,0);
+  analogWrite(r_in2,200);
+  analogWrite(l_in1,0);
+  analogWrite(l_in2,200);
+  
+  delay(1000);
+  
+  
+  
+}
